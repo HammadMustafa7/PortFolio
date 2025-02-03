ed9ce861acd01191fb10b37d7b970749b6818d9d
Hammad Mustafa <hammadmustafa8401@gmail.com>
2025-02-03 15:37:10 +0500

    vite configured

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/PortFolio/', // Keep this if you need a custom base path
+  base: '/PortFolio/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['react-router-dom'] // This is unlikely needed unless you're externally linking it
+    }
+  }
 });
