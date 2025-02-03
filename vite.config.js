972c6a6ec18cea89b04a816e9edcd2df4d08c502
Hammad Mustafa <hammadmustafa8401@gmail.com>
2025-02-03 15:34:40 +0500

    vite configured

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/PortFolio/',
+  base: '/PortFolio/', // Keep this if you need a custom base path
   plugins: [react()],
 });
